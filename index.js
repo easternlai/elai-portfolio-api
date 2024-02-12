2bc3a6f462fac05feb8f65d81b17ea490e77a4dd
Eastern <easternlai@hotmail.com>
2024-02-12 00:50:47 -0800

    added ingress.yaml

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV !== "production") {
 
 //CORS settup
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://www.easternlai.com");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
